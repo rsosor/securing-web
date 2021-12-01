da7783915a9e44a5c3851168979f34a3f67a0f79
RsosoR <godpkpk205@gmail.com>
2021-12-01 16:56:47 +0800

    first commit

---
+++ src/test/java/com/rsosor/securingweb/XadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsosor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
